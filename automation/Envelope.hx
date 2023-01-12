63b7534083d636bcac0700e63614d06a41558957
jf <22012032+jobf@users.noreply.github.com>
2023-01-12 15:48:49 +0000

    add sustain to Envelope

--- automation/Envelope.hx
+++ automation/Envelope.hx
@@ -15,8 +15,9 @@ class Envelope
 
 	var is_opening:Bool = false;
 	var is_closing:Bool = false;
-	var amplitude:Ramp;
-	var state:EnvelopeState = Closed;
+	public var amplitude:Ramp;
+	public var state(default, null):EnvelopeState = Closed;
+	public var has_sustain:Bool = true;
 
 	public function new(sampleRate:Int)
 	{
@@ -33,10 +34,18 @@ class Envelope
 	}
 
 
-	public function close()
+	public function close(?release:Float=null)
 	{
-		if(state == Sustain){
+		if(has_sustain){
 			is_closing = true;
+			trace('jump ramp end');
+		}
+		else{
+			state = Release;
+			trace('fall ramp end');
+			amplitude.stop();
+			is_closing = false;
+			// on_finished();
 		}
 	}
 
@@ -52,8 +61,16 @@ class Envelope
 			case Attack:
 				if (amplitude.isFinished())
 				{
-					state = Sustain;
-					trace('ramp start Sustain');
+					if(has_sustain){
+						state = Sustain;
+						trace('ramp start Sustain');
+					}
+					else{
+						state = Release;
+						trace('ramp start Release');
+						amplitude.setRamp(0.0001, releaseTime);
+						is_closing = false;
+					}
 				}
 			case Sustain:
 				if (is_closing)
@@ -84,6 +101,10 @@ class Envelope
 		state = Attack;
 		trace('ramp start Attack');
 		amplitude.setRamp(1.0, attackTime);
+	}
+
+	public function now() {
+		return amplitude.now();
 	}
 }
 
@@ -120,8 +141,21 @@ class Ramp
 		return currentValue;
 	}
 
+	public function now():Float{
+		return currentValue;
+	}
+
+	public function stop(){
+		samplesRemaining = 0;
+		// currentValue = 0;
+	}
+
 	public function isFinished()
 	{
 		return samplesRemaining <= 0;
+	}
+
+	public function get_time_remaining():Float {
+		return samplesRemaining * sampleRate;
 	}
 }
